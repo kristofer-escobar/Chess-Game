e5985a3a9886d6a49f8cccc3e4589539be0053ed
Kristofer S. Escobar <kescobar@precisioncare.com>
2013-12-11 03:46:19 -0500

    Attempting to queue up animations.

--- Chess/js/chessmoves.js
+++ Chess/js/chessmoves.js
@@ -32,7 +32,7 @@ var ChessMoves = function() {
 
             setTimeout(function() {
                 loadState(moveList, true);
-            }, 5000);
+            }, 2000);
 
             //loadState(moveList, true);
         } else {
@@ -68,7 +68,6 @@ var move = function(response) {
 
     moveList = response.moves;
 
-
     if (!gameover) { // using false for testing.
         // The game is over, show the final state of the board.
         loadState(moveList, false);
@@ -80,14 +79,6 @@ var move = function(response) {
 
     console.log(response);
 }
-
-// Function to replay an animated game.
-    function replay(moves) {
-        for (var i = 0; i < moves.length; i++) {
-
-        }
-
-    }
 
     function loadState(list, animate) {
 
@@ -119,43 +110,58 @@ var move = function(response) {
                 targetPosition = targetPosition.substring(3, 5);
             }
 
+            // Get the current piece being moved.
+            currentPiece = board.boardPosition[startPosition];
+
             // Checking for white castling.
             if (pieceName == 'K' && startPosition == 'E1' && (targetPosition == 'C1' || targetPosition == 'G1')) {
 
                 // Move right rook.
                 if (targetPosition == 'C1') {
-                    var rook = board.boardPosition['A1'];
-
-                    // Update the piece's position on the board.
-                    board.boardPosition['D1'] = rook;
 
                     if (animate) {
-                        movePiece(startPosition, targetPosition);
+                        animateQueue.push({
+                            func: movePiece,
+                            param1: 'A1',
+                            param2: 'D1'
+                        });
                     } else {
+                        
+                        var rook = board.boardPosition['A1'];
+
+                        // Update the piece's position on the board.
+                        board.boardPosition['D1'] = rook;
+
                         // Move the model.
                         rook.position.set(board.PositionMap['D1'].x, 0, board.PositionMap['D1'].z);
+
+                        // Clear out previous position.
+                        board.boardPosition['A1'] = "empty";
                     }
 
-                    // Clear out previous position.
-                    board.boardPosition['A1'] = "empty";
                 }
-
                 // Move left rook.
                 if (targetPosition == 'G1') {
-                    var rook = board.boardPosition['H1'];
-
-                    // Update the piece's position on the board.
-                    board.boardPosition['F1'] = rook;
 
                     if (animate) {
-                        movePiece(startPosition, targetPosition);
+                        animateQueue.push({
+                            func: movePiece,
+                            param1: 'H1',
+                            param2: 'F1'
+                        });
                     } else {
+                        var rook = board.boardPosition['H1'];
+
+                        // Update the piece's position on the board.
+                        board.boardPosition['F1'] = rook;
+
                         // Move the model.
                         rook.position.set(board.PositionMap['F1'].x, 0, board.PositionMap['F1'].z);
+                           
+                        // Clear out previous position.
+                        board.boardPosition['H1'] = "empty";
                     }
 
-                    // Clear out previous position.
-                    board.boardPosition['H1'] = "empty";
                 }
             }
 
@@ -164,72 +170,120 @@ var move = function(response) {
 
                 // Move right rook.
                 if (targetPosition == 'C8') {
-                    var rook = board.boardPosition['A8'];
-
-                    // Update the piece's position on the board.
-                    board.boardPosition['D8'] = rook;
 
                     if (animate) {
-                        movePiece(startPosition, targetPosition);
+                        animateQueue.push({
+                            func: movePiece,
+                            param1: 'A8',
+                            param2: 'D8'
+                        });
                     } else {
+                        var rook = board.boardPosition['A8'];
+
+                        // Update the piece's position on the board.
+                        board.boardPosition['D8'] = rook;                       
+                        
                         // Move the model.
                         rook.position.set(board.PositionMap['D8'].x, 0, board.PositionMap['D8'].z);
+                        
+                        // Clear out previous position.
+                        board.boardPosition['A8'] = "empty";
                     }
 
-                    // Clear out previous position.
-                    board.boardPosition['A8'] = "empty";
                 }
 
                 // Move left rook.
                 if (targetPosition == 'G8') {
-                    var rook = board.boardPosition['H8'];
-
-                    // Update the piece's position on the board.
-                    board.boardPosition['F8'] = rook;
 
                     if (animate) {
-                        movePiece(startPosition, targetPosition);
+                        animateQueue.push({
+                            func: movePiece,
+                            param1: 'H8',
+                            param2: 'F8'
+                        });
                     } else {
+                        var rook = board.boardPosition['H8'];
+
+                        // Update the piece's position on the board.
+                        board.boardPosition['F8'] = rook;
+
                         // Move the model.
                         rook.position.set(board.PositionMap['F8'].x, 0, board.PositionMap['F8'].z);
+
+                        // Clear out previous position.
+                        board.boardPosition['H8'] = "empty";
                     }
 
-                    // Clear out previous position.
-                    board.boardPosition['H8'] = "empty";
                 }
             }
 
-            // Get the current piece being moved.
-            currentPiece = board.boardPosition[startPosition];
-
+            // Check for captures.
             if (board.boardPosition[targetPosition] != 'empty') {
                 // Stored captured pieces to animate capture.
                 captured.push(board.boardPosition[targetPosition]);
 
-                // Shrink captured pieces.
-                board.boardPosition[targetPosition].scale.set(0.01, 0.01, 0.01);
-            }
+                if (animate) {
+                    animateQueue.push({
+                        func: capture,
+                        param1: targetPosition,
+                        param2: 0.01
+                    });
+                } else {
+                    // Shrink captured pieces.
+                    board.boardPosition[targetPosition].scale.set(0.01, 0.01, 0.01);
+                }
 
-            // Update the piece's position on the board.
-            board.boardPosition[targetPosition] = currentPiece;
+            }
 
             if (animate) {
-                movePiece(startPosition, targetPosition);
+                animateQueue.push({
+                    func: movePiece,
+                    param1: startPosition,
+                    param2: targetPosition
+                });
             } else {
+
+                // Update the piece's position on the board.
+                board.boardPosition[targetPosition] = currentPiece;
+
                 // Move the model.
                 currentPiece.position.set(board.PositionMap[targetPosition].x, 0, board.PositionMap[targetPosition].z);
-            }
 
-            // Clear out previous position.
-            board.boardPosition[startPosition] = "empty";
+                // Clear out previous position.
+                board.boardPosition[startPosition] = "empty";
+            }
+        }
+
+        // Check for animations.
+        if (animateQueue.length > 0) {
+            // There are animations to handle.
+            handleAnimations(animateQueue);
         }
     }
 
+    // Helper function to "clear" out chess board.
     function clearBoard() {
         for (var position in board.boardPosition) {
-            if(board.boardPosition[position] != 'empty'){
+            if (board.boardPosition[position] != 'empty') {
                 board.boardPosition[position].scale.set(0.01, 0.01, 0.01);
             }
-            
+
+        }
+    }
+
+    // Animate capturing of pieces.
+    function capture(){
+
+    }
+
+    // Function to handle queued up animations.
+    function handleAnimations( queue ){
+        while(queue.length > 0){
+            var animation = queue.shift();
+
+            setTimeout(function() {
+                // Run animation.
+                animation.func(animation.param1, animation.param2);
+            }, 3000);
         }
     }
